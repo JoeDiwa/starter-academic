e5a9257bb772cb25ebaa456d8f4b38863cf10ef7
Konso Wawa <wawakonso@gmail.com>
2020-11-24 02:03:42 +0100

    test js contact file

---
+++ assets/js/contact.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.querySelector('#contact .col-lg-8').className += ' col-lg-12';
+    document.querySelector('#contact ul.fa-ul').className += ' d-flex justify-content-between';
+}
